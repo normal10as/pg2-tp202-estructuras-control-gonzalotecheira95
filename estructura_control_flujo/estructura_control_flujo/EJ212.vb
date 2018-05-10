cf7a4f95a564954d67bfe20e3cdabe1d3f425f5d
Gonzalo Techeira <gnxa95@hotmail.com>
2018-05-09 22:25:31 -0300

    ej212:B

---
+++ estructura_control_flujo/estructura_control_flujo/EJ212.vb
@@ -0,0 +1,21 @@
+﻿Module EJ212
+    Sub Main()
+        Dim a As Long = 0
+        Dim b As Long = 1
+        Dim c As Long
+        Dim cc As Integer
+        cc = 0
+        Do
+            Console.WriteLine(c)
+            c = c + b
+
+            a = b
+            b = c
+            cc = cc + 1
+
+        Loop While cc <= 20
+
+        Console.ReadKey()
+
+    End Sub
+End Module
