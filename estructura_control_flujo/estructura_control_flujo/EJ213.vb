bd652a29aa622cd048c57f50a04e0e9979448e3c
Gonzalo Techeira <gnxa95@hotmail.com>
2018-05-13 15:50:51 -0300

    ...

---
+++ estructura_control_flujo/estructura_control_flujo/EJ213.vb
@@ -0,0 +1,17 @@
+﻿Module EJ213
+    Sub Main()
+        Dim a As Integer
+        Dim band As Boolean = True
+
+        Do While a < 1000
+            For i As Integer = 1 To a + 1
+                If (a Mod i = 0) Then
+                    a = a + 1
+                End If
+            Next i
+            Console.WriteLine(a)
+            a = a + 1
+        Loop
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ estructura_control_flujo/estructura_control_flujo/EJ214.vb
@@ -0,0 +1,18 @@
+﻿Module EJ214
+    Sub Main()
+        Dim cant, preunit, eval, ccc As Integer
+
+        ccc = 1
+        Console.WriteLine("Ingrese Cantidad de Productos a Evaluar")
+        eval = Console.ReadLine()
+        Do
+            Console.WriteLine("Producto: " & ccc)
+            Console.WriteLine("Ingresar Cantidad de Productos")
+            cant = Console.ReadLine()
+            Console.WriteLine("Ingresar Precio Unitario Prod.")
+            preunit = Console.ReadLine()
+
+            ccc = ccc + 1
+        Loop While (ccc < eval)
+    End Sub
+End Module
