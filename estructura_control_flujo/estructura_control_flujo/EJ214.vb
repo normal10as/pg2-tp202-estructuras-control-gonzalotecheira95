3d13d3973b86d3b7fcfabcc3c80489144d82b51a
Gonzalo Techeira <gnxa95@hotmail.com>
2018-05-25 00:41:50 -0300

    arreglos

--- estructura_control_flujo/estructura_control_flujo/EJ214.vb
+++ estructura_control_flujo/estructura_control_flujo/EJ214.vb
@@ -1,5 +1,39 @@
 ﻿Module EJ214
     Sub Main()
-        sss
+        Dim a, b, c, d, tot_desc, tot_cob, tot As Integer
+
+        Do
+            Console.WriteLine("ingrese cantidad de producto: ")
+            a = Console.ReadLine()
+            If a <> 0 Then
+                Console.WriteLine("ingrese precio del producto por unidad: ")
+                b = Console.ReadLine()
+            End If
+
+
+            d = (a * b)
+            tot = (d - c)
+            tot_cob = tot_cob + tot
+            Console.WriteLine("Sub Total" & vbCrLf & "Cantidad por precio unitario: " & d)
+            If (a >= 10) And (a <= 50) Then
+                c = d * 5 / 100
+                Console.WriteLine("Se desconto un 5%")
+            ElseIf (a >= 51) And (a <= 250) Then
+                c = d * 10 / 100
+                Console.WriteLine("Se desconto un 10%")
+            ElseIf (a >= 251) Then
+                c = d * 20 / 100
+                Console.WriteLine("Se desconto un 20%")
+            End If
+            tot_desc = tot_desc + c
+            Console.WriteLine("Monto descontado: " & c)
+            Console.WriteLine("Precio Total: " & tot)
+        Loop While (a <> 0)
+
+        Console.WriteLine("Descuento Total: " & tot_desc)
+        Console.WriteLine("Precio Total a cobrar: " & tot_cob)
+
+
+        Console.ReadKey()
     End Sub
 End Module
---
+++ estructura_control_flujo/estructura_control_flujo/EJ215.vb
@@ -0,0 +1,31 @@
+﻿Module EJ215
+
+    Friend Const interes As Decimal = 1.36
+    Sub Main()
+        Dim cant_inicial, cant_mes As Int64
+        Dim a, b, c As Integer
+
+        Console.WriteLine("Ingrese el monto inicial: ")
+        cant_inicial = Console.ReadLine()
+        Console.WriteLine("Ingrese la cantidad de meses: ")
+        cant_mes = Console.ReadLine()
+        a = cant_inicial
+
+
+        For i As Int16 = 1 To cant_mes
+            b = cant_inicial * (interes / 12)
+
+            c = cant_inicial + b
+            a += b
+
+
+            Console.WriteLine(i & "  " & a & " " & b & " " & a)
+
+        Next
+        Console.WriteLine(a)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
