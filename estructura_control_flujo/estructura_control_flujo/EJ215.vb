706d0aa50270c812950dfe06c2148b946b3c2800
Gonzalo Techeira <gnxa95@hotmail.com>
2018-05-27 18:57:42 -0300

    ultimoscambiosu.u

--- estructura_control_flujo/estructura_control_flujo/EJ215.vb
+++ estructura_control_flujo/estructura_control_flujo/EJ215.vb
@@ -2,7 +2,7 @@
 
     Friend Const interes As Decimal = 1.36
     Sub Main()
-        Dim cant_inicial, cant_mes As Int64
+        Dim cant_inicial, cant_mes, interess As Integer
         Dim a, b, c As Integer
 
         Console.WriteLine("Ingrese el monto inicial: ")
@@ -14,15 +14,16 @@
 
         For i As Int16 = 1 To cant_mes
             b = cant_inicial * (interes / 12)
+            a += c
+            c = a + b
 
-            c = cant_inicial + b
-            a += b
+            interess = interes + b
 
-
-            Console.WriteLine(i & "  " & a & " " & b & " " & a)
+            Console.WriteLine(i & "  " & a & " " & b & " " & c)
 
         Next
         Console.WriteLine(a)
+        Console.WriteLine(interess)
 
         Console.ReadKey()
 
